7b22406014005981bd613b1b02e26b42b05d4fad
Pooja08Sahu <ps9387250@gmail.com>
2025-12-17 19:11:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,101 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+
+
+
+<div class="container"> 
+    <footer class="py-5"> 
+        <div class="row"> 
+            <div class="col-6 col-md-2 mb-3"> 
+                <h5>Connect With Us</h5> 
+                <ul class="nav flex-column"> 
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-body-secondary">Facebook</a>
+                    </li> 
+                    <li class="nav-item mb-2">
+                        <a href="mailto:pooja_34162" class="nav-link p-0 text-body-secondary">Instagram</a>
+                    </li> 
+                    <li class="nav-item mb-2">
+                        <a href="mailto:9170981928" class="nav-link p-0 text-body-secondary">Whatsapp</a>
+                    </li> 
+                    <li class="nav-item mb-2">
+                        <a href="@Funzilla-w8i" class="nav-link p-0 text-body-secondary">Youtube</a>
+                    </li> 
+                     
+                </ul> 
+                </div> 
+                <div class="col-6 col-md-2 mb-3"> 
+                    <h5><Section></Section></h5> 
+                    <ul class="nav flex-column"> 
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link p-0 text-body-secondary">Home</a>
+                        </li> 
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link p-0 text-body-secondary">Features</a>
+                        </li> 
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link p-0 text-body-secondary">Pricing</a>
+                        </li> 
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link p-0 text-body-secondary">FAQs</a>
+                        </li> 
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link p-0 text-body-secondary">About</a>
+                        </li> 
+                    </ul> 
+                </div>
+                
+                
+                <div class="col-6 col-md-2 mb-3"> 
+                    <h5>Contact Details</h5> 
+                    <ul class="nav flex-column">
+                         <li class="nav-item mb-2">
+                            <a href="https://maps.google.com" class="nav-link p-0 text-body-secondary">Address: India, Utter Pradesh,Prayagraj</a>
+                        </li> 
+                         
+                        <li class="nav-item mb-2">
+                            <a href="tel-9170981928" class="nav-link p-0 text-body-secondary">Call+ 9170981928, 7318285461, 9161759031</a>
+                        </li> 
+                        <li class="nav-item mb-2">
+                            <a href="mailto-ps9387250@gmail.com" class="nav-link p-0 text-body-secondary">E-mail: ps9387250@gmail.com</a>
+                        </li> 
+                         
+                    </ul> 
+                </div> 
+                <div class="col-md-5 offset-md-1 mb-3"> 
+                    <form> <h5>Subscribe to our newsletter</h5> 
+                    <p>Monthly digest of what's new and exciting from us.</p> 
+                    <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                         <label for="newsletter1" class="visually-hidden">Email address</label> 
+                         <input id="newsletter1" type="email" class="form-control" placeholder="Email address"> 
+                         <button class="btn btn-primary" type="button">Subscribe</button> 
+                        </div> </form> 
+                    </div> </div> 
+                    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                         <p>© 2025 Company, Inc. All rights reserved.</p> 
+                         <ul class="list-unstyled d-flex"> <li class="ms-3">
+                            <a class="link-body-emphasis" href="#" aria-label="Instagram">
+                                <svg class="bi" width="24" height="24">
+                                    <use xlink:href="#instagram"></use>
+                                </svg></a></li> 
+                                <li class="ms-3"><a class="link-body-emphasis" href="#" aria-label="Facebook">
+                                    <svg class="bi" width="24" height="24" aria-hidden="true">
+                                        <use xlink:href="#facebook"></use>
+                                    </svg></a></li> 
+                                </ul> 
+                            </div> 
+                        </footer> </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
